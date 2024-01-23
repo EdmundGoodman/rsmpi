9b38f53f7b3ba867de81dafae51e80c4478fe53b
EdmundGoodman <egoodman3141@gmail.com>
2024-01-23 00:05:39 +0000

    Fix issue in unnecessary additional test case

--- examples/arrays.rs
+++ examples/arrays.rs
@@ -11,12 +11,4 @@ fn main() {
     world.this_process().send(&to_send);
     let (msg, _status) = future.get();
     assert_eq!(to_send, msg);
-
-    let mut msg = [0, 0, 0];
-    mpi::request::scope(|scope| {
-        let status = world.any_process().immediate_receive_into(scope, &mut msg);
-        world.this_process().send(&to_send);
-        println!("{:?}", status);
-    });
-    assert_eq!(to_send, msg);
 }
